4a6da32a39e910bb9d2d2079948b035211bba2d7
Giga Razki Arianda <razkigiga@email.com>
2024-06-02 14:45:08 +0700

    Update

--- src/component/Project/ProjectData.js
+++ src/component/Project/ProjectData.js
@@ -6,8 +6,12 @@ const projects = [
         ID: 'Sistem Kontrol Kipas Menggunakan Suara Multi-Bahasa Melalui Klasifikasi Audio pada Yamnet'
       },
       description: {
-        EN: `<p>This project is my final assignment as a requirement for obtaining a bachelors degree in engineering. This project has a function to turn on and off the fan using sounds from several languages ​​that have been trained via Python and communicated on the Arduino Uno using serial communication.</p>`,
-        ID: `<p>Proyek ini adalah tugas akhir saya sebagai syarat untuk memperoleh gelar sarjana teknik. Proyek ini memiliki fungsi untuk menyalakan dan mematikan kipas menggunakan suara dari beberapa bahasa yang telah dilatih melalui Python dan dikomunikasikan pada Arduino Uno menggunakan komunikasi serial.</p>`
+        EN: ` 
+             <p>This project is my final assignment as a requirement for obtaining a bachelors degree in engineering. This project has a function to turn on and off the fan using sounds from several languages ​​that have been trained via Python and communicated on the Arduino Uno using serial communication.</p>
+              `,
+        ID: `
+              <p>Proyek ini adalah tugas akhir saya sebagai syarat untuk memperoleh gelar sarjana teknik. Proyek ini memiliki fungsi untuk menyalakan dan mematikan kipas menggunakan suara dari beberapa bahasa yang telah dilatih melalui Python dan dikomunikasikan pada Arduino Uno menggunakan komunikasi serial.</p>
+              `
       },
       image: '/assets/project1.jpg',
       tag: 'Thesis',
